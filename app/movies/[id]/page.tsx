9a9cd5846b862e7ecab0c76788ab35af5ab537dc
minjae <minjae2271@naver.com>
2024-09-05 23:11:00 +0900

    metadata

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,6 +1,18 @@
 import { getMovie } from "actions/movieAction";
 import Ui from "./ui";
 
+export async function generateMetadata({ params }) {
+    const movie = await getMovie(params.id)
+
+    return {
+        title: movie.title,
+        description: movie.overview,
+        openGraph: {
+            images: [movie.image_url]
+        }
+    }
+}
+
 export default async function MovieDetail({ params }) {
     const movie = await getMovie(params.id)
 
